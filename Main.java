ccfbd89ac74b4c9831d609e6df1a7a2a4751cab9
Garvita12 <94907422+Garvita12@users.noreply.github.com>
2025-11-15 09:53:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,17 +6,23 @@ import com.fkwallet.service.impl.WalletServiceImpl;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        WalletServiceImpl walletService = new WalletServiceImpl(null);
+        WalletServiceImpl walletService = new WalletServiceImpl();
         OfferServiceImpl offerService = new OfferServiceImpl(walletService);
-        walletService = new WalletServiceImpl(offerService);
-        offerService = new OfferServiceImpl(walletService);
+        walletService.setOfferService(offerService);
+        walletService.createWallet("Harry", new BigDecimal("100"));
+        walletService.createWallet("Ron", new BigDecimal("95.7"));
+        walletService.createWallet("Hermione", new BigDecimal("104"));
+        walletService.createWallet("Albus", new BigDecimal("200"));
+        walletService.createWallet("Draco", new BigDecimal("500"));
 
-        var processor = new CommandProcessor(walletService);
+        CommandProcessor processor = new CommandProcessor(walletService);
 
-        System.out.println("FkRupee Wallet CLI. Enter commands. Type 'exit' to quit.");
+        System.out.println("FkRupee Wallet CLI demo. Type commands or 'exit' to quit.");
+        System.out.println("Sample: Overview");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while ((line = br.readLine()) != null) {
