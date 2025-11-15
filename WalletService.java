6fb3b636ff35e68430d01c61e4a6a797facb2930
Garvita12 <94907422+Garvita12@users.noreply.github.com>
2025-11-15 09:50:28 +0530

    Remove exception from transfer method signature

--- WalletService.java
+++ WalletService.java
@@ -3,12 +3,11 @@ package com.fkwallet.service;
 import com.fkwallet.model.Wallet;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Map;
 
 public interface WalletService {
     void createWallet(String owner, BigDecimal initial);
-    void transfer(String from, String to, BigDecimal amount) throws IllegalArgumentException;
+    void transfer(String from, String to, BigDecimal amount);
     String statement(String owner);
     Map<String, String> overview();
     void offer2();
